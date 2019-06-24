9d39577ebad98acd0f78b06d5e1f5e1d93118aba
2yan <2yan@outlook.com>
2019-06-24 09:28:29 -0700

    Removes gravity

--- sensors/accel.py
+++ sensors/accel.py
@@ -1,25 +1,58 @@
 from adxl345 import ADXL345
 import pandas as pd
 import numpy as np
-
+import time
+import threading
 
 print("Initalizing Accelerometer")
 s = ADXL345()
+callibration = {'x_accel':0,'y_accel':0, 'z_accel':0 }
 
+def callibrate(secs = 5):
+    print('callibrating Accelerometer')
+    global callibration
+    callibration = {'x_accel':0,'y_accel':0, 'z_accel':0 }
+ 
+
+    vals = []
+    start = time.monotonic()
+    while time.monotonic() <= (start  + secs):
+        vals.append(get_data(50, False))
+        x = pd.DataFrame(vals)
+        x =x.mean()
+        callibration = x.to_dict()
+    print("callibrated Accelerometer")
+    return vals
+
+def callibrate_async(secs = 5):
+    t = threading.Thread(target = callibrate, args = (secs,))
+    t.start()
+    return
 
 
 def raw_read():
     ans = s.get_axes(False)
-    x = ans['x']
-    y = ans['y']
+    x = ans['x'] 
+    y = ans['y'] 
     z = ans['z']
     return {'x_accel':x, 'y_accel':y, 'z_accel':z} 
 
 
+def correct(x):
+    global callibration
+    x['x_accel'] =  x['x_accel'] - callibration['x_accel']
+    x['y_accel'] =  x['y_accel'] - callibration['y_accel']
+    x['z_accel'] =  x['z_accel'] - callibration['z_accel']
+    return x
 
-def get_data(samples = 1):
+def get_data(samples = 1, remove_gravity = True):
+    
     if samples == 1:
-        return raw_read()
+        x = raw_read()
+        if remove_gravity:
+            x = correct(x)
+        return x
+    
     xs = []
     ys = []
     zs = []
@@ -37,11 +70,12 @@ def get_data(samples = 1):
     xs = np.mean(xs)
     ys = np.mean(ys)
     zs = np.mean(zs)
-    return {'x_accel':xs, 'y_accel':ys, 'z_accel':zs} 
+    final =  {'x_accel':xs, 'y_accel':ys, 'z_accel':zs} 
+    if remove_gravity:
+        final = correct(final)
+    return final
 
-
-     
-    
+callibrate_async(5) 
 
 if __name__ == '__main__':
 
