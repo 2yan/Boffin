160d59aaa3b31e46a77756f2038219459445733f
2yan <2yan@outlook.com>
2019-06-26 21:04:47 -0700

    Reduces obs before drawing for speed

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from display import Display
 
 
 
-
+print("LOADING DATA")
 data = reader.get_data()
 
-data = processor.reduce_obs(data, 1000)
+print("SMOOTHING GPS")
 data = processor.smooth_data(data)
+print("FITTING")
 data = processor.fit_to_universe(data)
 
+print("REDUCING OBS")
+data = processor.reduce_obs(data, 1000)
 
+print("DRAWING")
 d = Display()
-
 img = d.draw_data(data)
 img.save('result.png')
